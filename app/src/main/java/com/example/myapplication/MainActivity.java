06af543550d7f8c80cf54406e5b6282369c20d43
VDani3 <villacarmona.dani@gmail.com>
2023-10-02 16:01:35 +0200

    Programa 'numero random'  millorat i acabat (02/10/2023)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 randomNumber = r.nextInt(100)+1;
+                evaluacio.setText("Inserta un numero del 1 al 100");
+                Toast toast = Toast.makeText(getApplicationContext(), "Numero cambiado", Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
 
@@ -43,23 +46,26 @@ public class MainActivity extends AppCompatActivity {
 
                 String res = numField.getText().toString();
                 if (res.equals("")) {           //Si no posa res
-                    res = "1111";
-                }
-                int num = Integer.parseInt(res);
+                    evaluacio.setText("Tienes que poner un numero");
+                    Toast toast = Toast.makeText(getApplicationContext(), "No puede ser vacio", duration);
+                    toast.show();
+                } else {
+                    int num = Integer.parseInt(res);
 
-                if (num == randomNumber) {
-                    evaluacio.setText("Has adivinado el numero! ("+Integer.toString(num)+")");
-                    Toast toast = Toast.makeText(getApplicationContext(), text, duration);
-                    toast.show();
-                } else if (num < randomNumber) {
-                    evaluacio.setText("El numero es mes gran que "+Integer.toString(num));
-                    Toast toast = Toast.makeText(getApplicationContext(), "Mal, prueba otra vez", duration);
-                    toast.show();
-                } else if (num > randomNumber) {
-                    evaluacio.setText("El numero es mes petit que "+Integer.toString(num));
-                    Toast toast = Toast.makeText(getApplicationContext(), "Mal, prueba otra vez", duration);
-                    toast.show();
-                }
+                    if (num == randomNumber) {
+                        evaluacio.setText("Has adivinado el numero! ("+Integer.toString(num)+")");
+                        Toast toast = Toast.makeText(getApplicationContext(), text, duration);
+                        toast.show();
+                        mostrarAlerta();
+                    } else if (num < randomNumber) {
+                        evaluacio.setText("El numero es mes gran que "+Integer.toString(num));
+                        Toast toast = Toast.makeText(getApplicationContext(), "Mal, prueba otra vez", duration);
+                        toast.show();
+                    } else if (num > randomNumber) {
+                        evaluacio.setText("El numero es mes petit que "+Integer.toString(num));
+                        Toast toast = Toast.makeText(getApplicationContext(), "Mal, prueba otra vez", duration);
+                        toast.show();
+                }}
 
             }
         });
@@ -73,7 +79,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
-    
+
+    private void mostrarAlerta() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Buen trabajo");
+        builder.setMessage("Has acertado el numero!");
+        builder.setPositiveButton("Oky Dockie", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Acción a realizar al hacer clic en el botón "Aceptar"
+                dialog.dismiss(); // Cierra el AlertDialog
+            }
+        });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
 
 
 }
